5e1cac362a06ad154bfe0bf92a01003c84f197d4
herrinb <benjaminherring27@gmail.com>
2024-09-11 12:29:21 -0400

    final

--- Herring_Lab04/Herring_Lab04.cpp
+++ Herring_Lab04/Herring_Lab04.cpp
@@ -10,13 +10,84 @@ void printMenu() {
 	cout << "Your Selection: ";
 }
 void factorial() {
-	// I recommend writing your factorial code here
+	cout << "Factorial: " << endl;
+	int x;
+	cout << "Enter a number: ";
+	cin >> x;
+	while (x < 0) {
+		cout << "Nice try, please enter a positive number...: ";
+		cin >> x;
+	}
+	int factorial = 1;
+	cout << x << "! = ";
+	for (int i = 1; i <= x; i++) {
+		factorial *= i;
+		cout << i;
+		if (i < x) {
+			cout << " * ";
+		}
+	}
+	cout << " = " << factorial << endl;
+	
+	
+	
 }
 void arithmetic() {
-	// I recommend writing your arithmetic series code here
+	cout << "Arithmetic Series: " << endl;
+	int start;
+	cout << "Enter a number to start at: ";
+	cin >> start;
+	int diff;
+	cout << "Enter a number to add each time: ";
+	cin >> diff;
+	int num;
+	cout << "Enter the number of elements in the series: ";
+	cin >> num;
+	while (num < 0) {
+		cout << "Nice try, please enter a positive number...: ";
+		cin >> num;
+	}
+	int i = 0;
+	int sum = 0;
+	while (i < num) {
+		sum += start;
+		cout << start;
+		if (i < (num - 1)) {
+			cout << " + ";
+		}
+		start += diff;
+		i++;
+	}
+	cout << " = " << sum << endl;
 }
 void geometric() {
-	// I recommend writing your geometric series code here
+	cout << "Geometric Series: " << endl;
+	int start;
+	cout << "Enter a number to start at: ";
+	cin >> start;
+	int mult;
+	cout << "Enter a number to multiply by each time: ";
+	cin >> mult;
+	int num;
+	cout << "Enter the number of elements in the series: ";
+	cin >> num;
+	while (num < 0) {
+		cout << "Nice try, please enter a positive number...: ";
+		cin >> num;
+	}
+	int i = 0;
+	int sum = 0;
+	while (i < num) {
+		sum += start;
+		cout << start;
+		if (i < (num - 1)) {
+			cout << " + ";
+		}
+		start *= mult;
+		i++;
+	}
+	cout << " = " << sum << endl;
+
 }
 int main() {
 	int choice;
